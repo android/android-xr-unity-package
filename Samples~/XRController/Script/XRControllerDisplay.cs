4b650160734cfe20fdde85d77664729f40e6e498
hj.cpdm.son <hj.cpdm.son@samsung.com>
2025-03-05 10:43:09 +0900

    Update license header

--- Samples~/XRController/Script/XRControllerDisplay.cs
+++ Samples~/XRController/Script/XRControllerDisplay.cs
@@ -1,6 +1,6 @@
-// <copyright file="XRControllerDisplay.cs" company="Samsung Electronics Co.">
+// <copyright file="XRControllerDisplay.cs" company="Google LLC">
 //
-// Copyright 2025 Samsung Electronics Co.
+// Copyright 2025 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
